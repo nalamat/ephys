0fe8ef79da6572197f29bfa859315c95deb56719
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2020-11-10 23:44:46 -0500

    Condition ID fix for low-effort mode

--- getCondID.m
+++ getCondID.m
@@ -1,10 +1,13 @@
 function condID = getCondID(trialInfo, a)
-	if strcmpi(trialInfo.trialType(1:2), 'GO')
+	if strncmpi(trialInfo.trialType, 'nogo', 4) || ...
+			trialInfo.targetLevel == 0    % for low-effort mode
+		condID = 1;     % nogo is the first condition
+	elseif strncmpi(trialInfo.trialType, 'go', 2)
 		freqID  = find(a.targetFreqs  == trialInfo.targetFreq );
 		levelID = find(a.targetLevels == trialInfo.targetLevel);
 
 		condID = (freqID-1)*length(a.targetLevels)+levelID+1;
-	else    % nogo is the first condition
-		condID = 1;
+	else
+		error('[getCondID] Trial condition not defined');
 	end
 end
