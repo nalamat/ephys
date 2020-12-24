2ff3ab1f8478875f63e1e7d0b752f6edbe64b77f
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2020-12-24 18:52:08 -0500

    Default dim to first non-1 dimension

--- private/nansem.m
+++ private/nansem.m
@@ -1,6 +1,11 @@
 function sem = nansem(x, dim)
+% Calculate standard error of the mean omiting nans
+% If dim is not given, defaults to first dimension that it's not of size 1
 	if nargin<2
-		dim = 1;
+		dim = find(size(x) ~= 1, 1);
+    if isempty(dim)
+        dim = 1;
+    end
 	end
 	sem = nanstd(x, 0, dim) ./ sqrt(sum(~isnan(x), dim));
 end
--- private/sem.m
+++ private/sem.m
@@ -1,6 +1,11 @@
 function sem = sem(x, dim)
+% Calculate standard error of the mean across the given dimension
+% If dim is not given, defaults to first dimension that it's not of size 1
 	if nargin<2
-		dim = 1;
+		dim = find(size(x) ~= 1, 1);
+    if isempty(dim)
+        dim = 1;
+    end
 	end
 	sem = std(x, 0, dim) ./ sqrt(size(x, dim));
 end
